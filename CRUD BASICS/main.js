8f4f78648d0f8c1d885c282511eb1f322d3231f0
Narayan Bhattarai(ナラヤン) <webnbsr@gmail.com>
2024-05-13 02:06:15 +0900

    initial files

---
+++ CRUD BASICS/main.js
@@ -0,0 +1,40 @@
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let  msg=document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("button clicked");
+  formValidation();
+
+});
+
+let formValidation=()=>{
+if(input.value===""){
+    msg.innerHTML="post cannot be blank"
+}
+else{
+   msg.innerHTML="";
+   acceptData();
+}
+};
+let data={};
+
+let acceptData=()=>{
+    data["text"]=input.value;
+    console.log(data);
+    createPost();
+}
+
+let createPost=()=>{
+  posts.innerHTML += `
+  <div>
+      <p>${data.text}</p>
+        <span class="options">
+              <i class="fa-solid fa-edit"></i>
+              <i class="fa-solid fa-trash"></i>
+        </span>
+    </div>
+  `;
+};
